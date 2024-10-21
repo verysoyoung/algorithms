fa4ed2435b346d72e8152c39dc90452a9aa32ffe
soyoung <duddlwwkd1@gmail.com>
2024-10-22 07:38:02 +0900

    feat: Queue 안 보고 복습

--- queue.js
+++ queue.js
@@ -1,58 +1,63 @@
-const arr = [1, 2, 3, 4, 5];
-arr.shift();
+const arr = [1, 2, 3, 4, 5]
+arr.shift()
 // FIFO 원칙, 선입선출 원칙
 // 배열을 이용하면 한 번 순회후 '위치 조정시간'이 추가로 필요해서 다른 언어보다 시간복잡도가 올라간다.
+// 자바스크립트에서는 큐와 관련된 객체가 내장되어있지 않다. 큐를 이용하기 위해서는 자체 구현이 필요하다.
 
-class Queue {
+export class Queue {
   constructor() {
-    this.storage = {};
-    this.front = 0;
-    this.rear = 0;
+    this.storage = {}
+    this.front = 0
+    this.rear = 0
   }
   //크기구하기
   size() {
     if (this.storage[this.rear] === undefined) {
-      return 0;
+      return 0
     } else {
-      return this.rear - this.front + 1;
+      return this.rear - this.front + 1
     }
   }
   add(value) {
     if (this.size() === 0) {
-      this.storage["0"] = value;
+      this.storage['0'] = value
     } else {
-      this.rear += 1;
-      this.storage[this.rear] = value;
+      this.rear += 1
+      this.storage[this.rear] = value
     }
   }
   popleft() {
-    let temp;
+    let temp
     //데이터가 한 개 남은경우. front, rear 위치가 같은경우
     if (this.front === this.rear) {
-      temp = this.storage[this.front];
-      delete this.storage[this.front];
-      this.front = 0;
-      this.rear = 0;
+      temp = this.storage[this.front]
+      delete this.storage[this.front]
+      this.front = 0
+      this.rear = 0
     } else {
-      temp = this.storage[this.front];
-      delete this.storage[this.front];
-      this.front += 1;
+      temp = this.storage[this.front]
+      delete this.storage[this.front]
+      this.front += 1
     }
-    return temp;
+    return temp
   }
 }
 
-const example = new Queue();
-example.add(12);
-example.add(1);
-example.add(2);
-example.add(3);
-example.add(4);
+const example = new Queue()
+example.add(12)
+console.log('example 1 :', example, example.size())
 
-console.log("example 1 :", example, example.size());
+example.add(1)
+example.add(2)
+example.add(3)
+example.add(4)
+console.log('example 1 :', example, example.size())
 
-example.popleft();
-console.log("example 2 :", example, example.size());
+example.popleft()
+console.log('example 2 :', example, example.size())
 
-example.popleft();
-console.log("example 3 :", example, example.size());
+example.popleft()
+example.popleft()
+example.popleft()
+example.popleft()
+console.log('example 3 :', example, example.size())
---
+++ queue2.js
@@ -0,0 +1,41 @@
+class Queue {
+  constructor() {
+    this.storage = {}
+    this.front = 0
+    this.rear = 0
+  }
+  add(value) {
+    if (this.size() === 0) {
+      this.storage['0'] = value
+    } else {
+      this.rear += 1
+      this.storage[this.rear] = value
+    }
+  }
+  popLeft() {
+    let temp
+    if (this.front === this.rear) {
+      temp = this.storage[this.front]
+      delete this.storage[this.front]
+      this.front = 0
+      this.rear = 0
+    } else {
+      temp = this.storage[this.front]
+      delete this.storage[this.front]
+      this.front += 1
+    }
+    return temp
+  }
+  size() {
+    if (this.storage[this.rear] === undefined) {
+      return 0
+    } else {
+      return this.rear - this.front + 1
+    }
+  }
+}
+
+const test = new Queue()
+test.add(1)
+test.add(1)
+console.log(test)
