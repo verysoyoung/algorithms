a9ebaa3248a64c0f34d93294a027f1e58e161213
soyoung <duddlwwkd1@gmail.com>
2024-10-23 08:48:15 +0900

    feat: baekjoon test 8393 다른식 (등차수열의 합)

--- javascript/baekJoon/8393.js
+++ javascript/baekJoon/8393.js
@@ -1,6 +1,9 @@
-const value = 4
+const n = 4
 
-let data = new Array(Number(value)).fill(0)
+let data = new Array(Number(n)).fill(0)
 let answer = data.map((_, i) => i + 1).reduce((acc, cur) => acc + cur)
 
 console.log(answer)
+
+//등차수열의 합 공식
+console.log((n * (n + 1)) / 2)
