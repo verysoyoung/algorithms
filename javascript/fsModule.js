348451e451c93efabc0b5db62c3dc68448e90873
soyoung <duddlwwkd1@gmail.com>
2024-10-23 07:38:18 +0900

    f feat: fs모듈 복사가능하게 수정

--- javascript/fsModule.js
+++ javascript/fsModule.js
@@ -1,6 +1,5 @@
 //파일시스템 모듈
 
 let fs = require('fs')
-let input = fs.readFileSync('input.txt').toString().split('\n')
-
+let input = fs.readFileSync('/dev/stdin').toString().split('\n')
 console.log(input)
