c3fa91915ed5513e5c9808fb40c6db4dc74228fa
soyoung <duddlwwkd1@gmail.com>
2024-10-24 07:12:26 +0900

    feat: baekjoon test 85568550 정수범위

--- javascript/baekJoon/85568550.js
+++ javascript/baekJoon/85568550.js
@@ -3,9 +3,9 @@ const n = 5
 const arr = [1, 34, 56, 4333, 23]
 console.log(`${Math.min(...arr)} ${Math.max(...arr)}`)
 
-let minValue = 1000001
-let maxValue = -1000001
-
+//모든 정수는 - 1000000 보다 크거나 같고 1000000 작거나 같은 정수
+let minValue = 1000001 //큰 수로 초기화
+let maxValue = -1000001 //작은 수로 초기화
 for (let i = 0; i < n; i++) {
   if (minValue > arr[i]) minValue = arr[i]
   if (maxValue < arr[i]) maxValue = arr[i]
