4fd7094e3516eeb0f55895d98d819ab861a59a3d
soyoung <duddlwwkd1@gmail.com>
2024-10-23 22:41:47 +0900

    feat: baekjoon test 2438 2중 for문

--- javascript/baekJoon/2438.js
+++ javascript/baekJoon/2438.js
@@ -2,3 +2,12 @@ const n = 5
 for (let i = 1; n >= i; i++) {
   console.log('*'.repeat(i))
 }
+
+let result = ''
+for (let i = 0; i < n; i++) {
+  for (let j = 0; j <= i; j++) {
+    result += '*'
+  }
+  result += '\n'
+}
+console.log(result)
