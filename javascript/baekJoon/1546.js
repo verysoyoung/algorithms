05c4864b927c0bff239c5600ec787392a9c6b49d
soyoung <duddlwwkd1@gmail.com>
2024-10-24 22:51:34 +0900

    feat: Max 값 구하는 방법 추가

--- javascript/baekJoon/1546.js
+++ javascript/baekJoon/1546.js
@@ -1,9 +1,11 @@
 //평균값 구하기
 
-const n = Number(input[0])
-const data = input[1].split(' ').map(Number)
+const n = 6
+const data = [90, 67, 56, 45, 34, 99]
 
 const max = Math.max(...data)
+const max2 = data.reduce((a, b) => Math.max(a, b))
+console.log(max, max2)
 const result =
   data.map(num => (num / max) * 100).reduce((acc, cur) => acc + cur) / n
 console.log(result)
