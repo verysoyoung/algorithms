20b97243f41a2d6c49e490c6f906b1e02ef20f8d
soyoung <duddlwwkd1@gmail.com>
2024-10-25 08:02:51 +0900

    feat: baekjoon test 1316 한 번 더 체크필요한 문제

---
+++ javascript/baekJoon/1316_check.js
@@ -0,0 +1,19 @@
+//a-z 까지 총 26개의 알파베잇 있다.
+//각 알파벳에 대하여 연속해서 등장한 횟수가 1번인지 체크한다.
+
+//그룹 단어인지 체크하는 함수
+const input = 'ccazzzzbb'
+function check(data) {
+  let setData = new Set(data[0])
+  for (let i = 0; i < data.length - 1; i++) {
+    if (data[i] !== data[i + 1]) {
+      if (setData.has(data[i + 1])) {
+        return false
+      } else {
+        setData.add(data[i + 1])
+      }
+    }
+  }
+  return true
+}
+console.log(check(input))
--- javascript/baekJoon/fsModule.js
+++ javascript/baekJoon/fsModule.js
@@ -1,5 +1,5 @@
 //파일시스템 모듈
 
 let fs = require('fs')
-let input = fs.readFileSync('/dev/stdin').toString().split('\n')
+let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
 console.log(input)
