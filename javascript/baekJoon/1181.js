5f29247c5dbec98c298e5b572d71064366e2ca50
soyoung <duddlwwkd1@gmail.com>
2024-10-29 23:13:14 +0900

    feat: sort 사전 순 정렬

--- javascript/baekJoon/1181.js
+++ javascript/baekJoon/1181.js
@@ -2,10 +2,16 @@ let [n, ...arr] = input
 
 const set = new Set(arr)
 const data = [...set]
-const arrrrr = data.sort()
+// const arrrrr = data.sort()
 const result = arrrrr.sort((a, b) => compare(a, b))
 function compare(a, b) {
-  return a.length - b.length
+  if (a.length != b.length) return a.length - b.length
+  else {
+    //길이가 같을 경우 사전 순으로 정렬
+    if (a < b) return -1
+    else if (a > b) return 1
+    else return 0
+  }
 }
 let answer = ''
 for (let point of result) {
