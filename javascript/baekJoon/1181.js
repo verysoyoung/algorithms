5cd80555b078524415c968768d18569419eab3b8
soyoung <duddlwwkd1@gmail.com>
2024-10-29 23:16:20 +0900

    feat: set 코드 짧게 수정

--- javascript/baekJoon/1181.js
+++ javascript/baekJoon/1181.js
@@ -1,9 +1,7 @@
 let [n, ...arr] = input
 
-const set = new Set(arr)
-const data = [...set]
-// const arrrrr = data.sort()
-const result = arrrrr.sort((a, b) => compare(a, b))
+const data = [...new Set(arr)]
+const result = data.sort((a, b) => compare(a, b))
 function compare(a, b) {
   if (a.length != b.length) return a.length - b.length
   else {
