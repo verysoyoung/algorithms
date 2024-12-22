642563e6bdadba511a849851fc65ccf1b0de2a93
soyoung <duddlwwkd1@gmail.com>
2024-12-23 06:25:51 +0900

    feat: BackJoon test 11399 다른방식으로 풀기

--- javascript/baekJoon/11399.js
+++ javascript/baekJoon/11399.js
@@ -1,11 +1,8 @@
 const fs = require('fs')
 let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
 let n = Number(input[0])
-let list1 = input[1].split(' ')
-let arr = []
-for (let i = 0; i < n; i++) {
-  arr.push(Number(list1[i]))
-}
+let arr = input[1].split(' ').map(Number)
+
 let list = arr.sort((a, b) => a - b)
 let amount = 0
 let count = 0
@@ -14,3 +11,11 @@ for (let i = 0; i < n; i++) {
   amount += count
 }
 console.log(amount)
+
+let answer = 0
+let summary = 0
+for (let i = 0; i < n; i++) {
+  summary += list[i] //i번째 사람이 기다린 총 시간
+  answer += summary //지금까지 소요된 총 시간
+}
+console.log(answer)
