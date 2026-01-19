acd55e92bdc9eaf46fc4f7a2c00d8f64e555afa3
Hong Soyoung <50602551+verysoyoung@users.noreply.github.com>
2026-01-19 09:59:56 +0900

    Update P739.ts

--- leetcode/P739.ts
+++ leetcode/P739.ts
@@ -4,7 +4,7 @@ function dailyTemperatures(temperatures: number[]): number[] {
     const stack:number[] = [];
     for(let i = 0; i < n; i++){
         while(stack.length > 0 && temperatures[i] > temperatures[stack[stack.length - 1]]){
-            const j = stack.pop()!;
+            const j = stack.pop();
             if (j === undefined) throw new Error("empty stack"); 
             answer[j] = i - j;
         }
