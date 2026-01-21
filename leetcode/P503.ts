e4949a82f363628a047881910dddba83a0a7dc06
Hong Soyoung <50602551+verysoyoung@users.noreply.github.com>
2026-01-21 21:51:12 +0900

    Update P503.ts

--- leetcode/P503.ts
+++ leetcode/P503.ts
@@ -1,15 +1,15 @@
 function nextGreaterElements(nums: number[]): number[] {
     const n = nums.length;
     const answer = Array<number>(n).fill(-1); 
-    const stack:number[] = [];
-    for(let i=0; i < n * 2; i++){
+    const stack: number[] = [];
+    for(let i = 0; i < n * 2; i++){
         const idx = i % n;
         while(stack.length > 0 && nums[idx] > nums[stack[stack.length - 1]]){
             const prevIdx = stack.pop();
             if(prevIdx === undefined) throw new Error('stack empty')
             answer[prevIdx] = nums[idx]
         }
-        if(i<n){
+        if(i < n){
             stack.push(i);
         }
     }
